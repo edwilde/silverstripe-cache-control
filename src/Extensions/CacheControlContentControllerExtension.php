563170479bbbf1e3e5de53cbb7c3e56e34502bc1
Ed Wilde <github@edwilde.com>
2025-12-17 11:11:58 +1300

    fix: use HTTPCacheControlMiddleware singleton properly
    Use the HTTPCacheControlMiddleware API directly:
    - publicCache() / privateCache()
    - setMaxAge()
    - enableCache() / disableCache()
    - setMustRevalidate()
    
    This is the proper SilverStripe way to set cache headers
    that will work with the middleware.

--- src/Extensions/CacheControlContentControllerExtension.php
+++ src/Extensions/CacheControlContentControllerExtension.php
@@ -23,31 +23,33 @@ class CacheControlContentControllerExtension extends Extension
     
     private function applyCacheControl($header)
     {
-        // Parse the header (e.g. "public, max-age=999")
-        $directives = array_map('trim', explode(',', $header));
-        
-        foreach ($directives as $directive) {
-            if (strpos($directive, 'max-age=') === 0) {
-                $maxAge = (int)substr($directive, 8);
-                HTTP::set_cache_age($maxAge);
-            } elseif ($directive === 'public') {
-                HTTP::set_cache_age(0); // Will be set by max-age
-            } elseif ($directive === 'private') {
-                HTTP::set_cache_age(0);
-                HTTP::privateCache();
-            } elseif ($directive === 'no-store') {
-                HTTP::set_cache_age(0);
-                $this->owner->getResponse()->addHeader('Cache-Control', 'no-store');
-            } elseif ($directive === 'must-revalidate') {
-                HTTP::set_cache_age(0);
-                $this->owner->getResponse()->addHeader('Cache-Control', 'must-revalidate');
-            }
+        // Parse the header to get max-age
+        $maxAge = 0;
+        if (preg_match('/max-age=(\d+)/', $header, $matches)) {
+            $maxAge = (int)$matches[1];
         }
         
-        // Now apply the headers
-        HTTP::add_cache_headers($this->owner->getResponse());
+        // Use HTTPCacheControlMiddleware to enable caching
+        $cacheControl = \SilverStripe\Control\Middleware\HTTPCacheControlMiddleware::singleton();
         
-        // Finally, override with our exact header
-        $this->owner->getResponse()->addHeader('Cache-Control', $header);
+        if (strpos($header, 'private') !== false) {
+            $cacheControl->privateCache();
+        } else {
+            $cacheControl->publicCache();
+        }
+        
+        if ($maxAge > 0) {
+            $cacheControl->setMaxAge($maxAge);
+        }
+        
+        if (strpos($header, 'must-revalidate') !== false) {
+            $cacheControl->setMustRevalidate(true);
+        }
+        
+        if (strpos($header, 'no-store') !== false) {
+            $cacheControl->disableCache(true);
+        } else {
+            $cacheControl->enableCache(true);
+        }
     }
 }
